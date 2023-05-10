db1ca8e1a29ea46034a6720731310ab58412a177
Nikola Bucan <nikolabucan@invoitix.com>
2023-05-10 11:58:05 +0200

    Finished

---
+++ eTickets/Models/Producer.cs
@@ -0,0 +1,27 @@
+﻿using eTickets.Data.Base;
+using System.ComponentModel.DataAnnotations;
+
+namespace eTickets.Models
+{
+    public class Producer:IEntityBase
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name = "Profile Picture")]
+        [Required(ErrorMessage ="Profile Picture is Required")]
+        public string ProfilePictureURL { get; set; }
+        [Display(Name = "Full Name")]
+        [Required(ErrorMessage = "Full Name is Required")]
+
+        public string FullName { get; set; }
+        [Display(Name = "Biography")]
+        [Required(ErrorMessage = "Biography is Required")]
+
+        public string Bio { get; set; }
+
+        //Relationships
+
+        public List<Movie> Movies { get; set; }
+    }
+}
--- eTickets/Program.cs
+++ eTickets/Program.cs
@@ -1,6 +1,36 @@
+using eTickets.Data;
+using eTickets.Data.Cart;
+using eTickets.Data.Services;
+using eTickets.Models;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnectionString");
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connectionString));
+//builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnectionString")));
+//Services Configuration
+builder.Services.AddScoped<IActorsService, ActorsService>();
+builder.Services.AddScoped<IProducersService, ProducersService>();
+builder.Services.AddScoped<ICinemasService, CinemasService>();
+builder.Services.AddScoped<IMoviesService, MoviesService>();
+builder.Services.AddScoped<IOrdersService, OrdersService>();
+
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+builder.Services.AddScoped(sc => ShoppingCart.GetShoppingCart(sc));
+
+//Authentication
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFrameworkStores<AppDbContext>();
+builder.Services.AddMemoryCache();
+builder.Services.AddSession();
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+});
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
@@ -17,11 +47,24 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseSession();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.UseAuthorization();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+});
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+//seed database
+AppDbInitializer.Seed(app);
+AppDbInitializer.SeedUsersAndRolesAsync(app).Wait();
 
 app.Run();
